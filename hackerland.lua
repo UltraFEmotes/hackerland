0da88fa69b6e781f92a3e15189e29b2a6c505f31
UltraFEmotes <manolisemail87+scripts@gmail.com>
2025-10-25 03:40:01 +1100

    Update hackerland.lua

--- hackerland.lua
+++ hackerland.lua
@@ -1,2 +1,2 @@
 setclipboard("https://discord.gg/9HsQvFN7v3")
-game.Players.LocalPlayer:Kick("Project: Borderlands is done\nGame over...\nConsider joining the official Project: Borderlands server, it has been copied to your clipboard!")
+game.Players.LocalPlayer:Kick("Project: Hackerlands is done\nGame over...\nConsider joining the official Project: Borderlands server, it has been copied to your clipboard!")
